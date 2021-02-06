01af833610f418bebece6b6e9df0d52693827a5e
松本一将 <k.matsumoto@benjmain.co.jp>
2021-02-06 17:33:30 +0900

    pushテスト

--- app.ts
+++ app.ts
@@ -1,9 +1,31 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+const person = {
   name: 'yota',
   age: 30,
-}
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
+};
+
+// person.role.push('admin'); 許可される
+// person.role[1] = 10; 許可されない
+// person.role = [0, 'admin', 'user']; 許可されない
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports']
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
